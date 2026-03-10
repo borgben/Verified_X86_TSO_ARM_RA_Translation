102569141d77a56a4c5e937afcff57cec42a490c
ZtereoHYPE <57519662+ZtereoHYPE@users.noreply.github.com>
2026-03-10 17:07:14 +0100

    Define x86 label

---
+++ LabelX86.v
@@ -0,0 +1,33 @@
+(* ob = (bob ∪ rfe ∪ moe ∪ fre)+ *)
+(* bob = ((R_acq_pc ; po) ∪ (po ; W_rel))*)
+
+(* ob is irreflexive *)
+
+From RelAcqProof Require Import Label. 
+
+Inductive LabelX86 := 
+| W_x86 (loc:Location) (val:Value)
+| R_x86 (loc:Location) (val:Value). 
+
+(* Instance LabelClassArm: LabelClass LabelArm.  *)
+Instance LabelClassX86: LabelClass LabelX86 := {
+    lab_loc l := match l with
+                | W_x86 loc _ => loc 
+                | R_x86 loc _ => loc 
+                end;
+      
+    lab_val l := match l with
+                | W_x86 _ val => val 
+                | R_x86 _ val => val 
+                end;
+
+    is_r l := match l with
+                | W_x86 _ _ => True 
+                | R_x86 _ _ => False 
+                end;
+
+    is_w l := match l with
+                | W_x86 _ _ => False 
+                | R_x86 _ _ => True 
+                end;
+}. 
