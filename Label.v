83c5ec7941873af4f60beff1af9a742c1be19812
ZtereoHYPE <57519662+ZtereoHYPE@users.noreply.github.com>
2026-03-10 19:45:56 +0100

    coherence and atomicity axioms!

--- Label.v
+++ Label.v
@@ -1,7 +1,7 @@
 Definition Location := nat.
 Definition Value := nat.
 
-Class LabelClass (L : Type):Type := {
+Class LabelClass (L : Type): Type := {
   lab_loc : L -> Location;
   lab_val : L -> Value; 
   is_r : L -> Prop;
