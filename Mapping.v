006adee86161615e88308c771309c8d75cba5830
ben <benjaminborg9@gmail.com>
2026-03-11 16:34:01 +0100

    Mapping

--- Mapping.v
+++ Mapping.v
@@ -4,6 +4,8 @@ From RelAcqProof Require Import Label.
 From RelAcqProof Require Import LabelArm.
 From RelAcqProof Require Import LabelX86. 
 
+
+(* *************************** Map from X86 to Arm ************************* *)
 Definition map_label_X86_Arm (lab_X86: LabelX86): LabelArm := 
 match lab_X86 with
 | W_x86 loc val => W_Rel loc val 
@@ -16,4 +18,32 @@ match  event_X86 with
 | EventThread uid tid lab => EventThread uid tid (map_label_X86_Arm lab)
 end. 
 
-Definition map_exec_X86_Arm (execX86:@Execution LabelX86 LabelClassX86):@Execution LabelArm LabelClassArm := . 
+Definition map_exec_X86_Arm (execX86:@Execution LabelX86 LabelClassX86):@Execution LabelArm LabelClassArm := {|
+    events := fun e => exists e', events execX86 e' /\ e = map_event_X86_Arm e';
+    po     := fun e1 e2 => exists x y, po execX86 x y /\ e1 = map_event_X86_Arm x /\ e2 = map_event_X86_Arm y;
+    rf     := fun e1 e2 => exists x y, rf execX86 x y /\ e1 = map_event_X86_Arm x /\ e2 = map_event_X86_Arm y;
+    mo     := fun e1 e2 => exists x y, mo execX86 x y /\ e1 = map_event_X86_Arm x /\ e2 = map_event_X86_Arm y; 
+    rmw    := fun e1 e2 => exists x y, rmw execX86 x y /\ e1 = map_event_X86_Arm x /\ e2 = map_event_X86_Arm y;  
+|}. 
+
+(* ************************** Map from Arm to X86 *************************** *)
+Definition map_label_Arm_X86 (lab_Arm: LabelArm): LabelX86 := 
+match lab_Arm with
+| W_Rel loc val => W_x86 loc val  
+| R_Acq_Pc loc val => R_x86 loc val   
+end. 
+
+Definition map_event_Arm_X86 (event_Arm:@Event LabelArm LabelClassArm):@Event LabelX86 LabelClassX86 := 
+match  event_Arm with 
+| EventInit uid lab => EventInit uid (map_label_Arm_X86 lab)
+| EventThread uid tid lab => EventThread uid tid (map_label_Arm_X86 lab)
+end. 
+
+Definition map_exec_Arm_X86 (execArm:@Execution LabelArm LabelClassArm):@Execution LabelX86 LabelClassX86 := {|
+    events := fun e => exists e', events execArm e' /\ e = map_event_Arm_X86 e';
+    po     := fun e1 e2 => exists x y, po execArm x y /\ e1 = map_event_Arm_X86 x /\ e2 = map_event_Arm_X86 y;
+    rf     := fun e1 e2 => exists x y, rf execArm x y /\ e1 = map_event_Arm_X86 x /\ e2 = map_event_Arm_X86 y;
+    mo     := fun e1 e2 => exists x y, mo execArm x y /\ e1 = map_event_Arm_X86 x /\ e2 = map_event_Arm_X86 y; 
+    rmw    := fun e1 e2 => exists x y, rmw execArm x y /\ e1 = map_event_Arm_X86 x /\ e2 = map_event_Arm_X86 y;  
+|}. 
+
